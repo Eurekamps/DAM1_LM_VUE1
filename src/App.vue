19a312868eee113bac10e7a811a3b845d63b420d
Eurekamps <4yony4@gmail.com>
2024-10-29 10:46:50 +0100

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Principal from './components/Principal.vue'
+</script>
+
+<template>
+
+  <Principal/>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Principal.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+
+    data() {
+      return {
+        message: 'Hello, Vue!'
+      };
+    },
+    methods: {
+      changeMessage() {
+        this.message = 'You clicked the button!';
+      }
+    }
+
+};
+</script>
+
+
+<template>
+    <h1> HOLA ESTA ES LA PAGINA PRINCIPAL</h1>
+    <p>Que tal es una app bla bla </p>
+
+    <h1>{{ message }}</h1>
+    <button @click="changeMessage">Click Me</button>
+    
+</template>
+
+
+<style scoped>
+
+</style>
