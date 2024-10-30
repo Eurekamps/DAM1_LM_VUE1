8898775e7a4db67c8d44ebc4eb85ca88cf199d4e
Eurekamps <4yony4@gmail.com>
2024-10-30 12:21:46 +0100

    Pagina login finalizada

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,7 +1,14 @@
 <script setup>
+import {ref} from 'vue';
+
+const email=ref('');
+const password=ref('');
+
+function clickLogear(){
+    window.alert("USUARIO: "+email + "PASSWORD: "+password);
+}
 
 </script>
-
 
 <template>
     <h1> LOGIN PAGE</h1><br/><br/>
@@ -17,7 +24,9 @@
         <label for="password">Password:</label>
         <input type="password" id="password" v-model="password" />
     </div>
+    <br/>
 
+    <button @click="clickLogear">Logearme</button>
 
 
 </template>
