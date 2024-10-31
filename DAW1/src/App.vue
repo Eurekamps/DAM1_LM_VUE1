d4a7922702c1a61a87bc8efff4e1d4cfe1002bcc
Eurekamps <4yony4@gmail.com>
2024-10-31 13:37:26 +0100

    Reorganizado codigo para clases

---
+++ DAW1/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+  import Principal from './components/Principal.vue';
+
+</script>
+
+<template>
+
+  <div>
+    <h1> MI PRIMERA APP</h1>
+  </div>
+  
+  <br/>
+  <br/>
+
+  <div>
+    <Principal></Principal>
+  </div>
+  
+
+</template>
+
+<style scoped>
+</style>
---
+++ DAW1/src/components/Principal.vue
@@ -0,0 +1,22 @@
+<script setup>
+</script>
+
+<template>
+
+    <h1> LOGIN</h1>
+
+    <div>
+        <label>USUARIO:</label>
+        <input></input>
+    </div>
+
+    <div>
+        <label>PASSWORD:</label>
+        <input></input>
+    </div>
+
+
+</template>
+
+<style scoped>
+</style>
