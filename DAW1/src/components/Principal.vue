adff2549a9d6654411fd929495ff39c22fceb72e
Eurekamps <4yony4@gmail.com>
2024-11-04 09:01:50 +0100

    nuevos cambios

--- DAW1/src/components/Principal.vue
+++ DAW1/src/components/Principal.vue
@@ -1,15 +1,24 @@
 <script setup>
     import { ref } from 'vue'
 
-    const sUsuario=ref('...')
+    const sUsuario=ref('')
     const sPassword=ref('')
+
+    const sUsuarioRe=ref('')
+    const sPasswordRe=ref('')
+    const sRepetirPasswordRe=ref('')
+
+    const blLoginVisible=ref(true);
+    const blRegistroVisible=ref(false);
 
     function presioneLogin(){
         window.alert("LOGIN CON USUARIO: "+sUsuario.value+" PASSWORD: "+sPassword.value)
     }
 
     function presioneRegistrar(){
-        window.alert("HAN PRESIONADO BOTON REGISTRAR")
+        //window.alert("HAN PRESIONADO BOTON REGISTRAR")
+        blLoginVisible.value=false;
+        blRegistroVisible.value=true;
     }
 
 
@@ -17,23 +26,52 @@
 
 <template>
 
-    <h1> LOGIN</h1>
+    <div id="contenedor-login" v-if="blLoginVisible">
+        <h1> LOGIN</h1>
 
-    <div>
-        <label>USUARIO:</label>
-        <input v-model="sUsuario" type="text"></input>
+        <div>
+            <label>USUARIO:</label>
+            <input v-model="sUsuario" type="text"></input>
+        </div>
+
+        <div>
+            <label>PASSWORD:</label>
+            <input v-model="sPassword" type="password"></input>
+        </div>
+
+        <button @click="presioneLogin">LOGEARSE</button>
+        <button @click="presioneRegistrar">REGISTRARSE</button>
     </div>
 
-    <div>
-        <label>PASSWORD:</label>
-        <input v-model="sPassword" type="password"></input>
-    </div>
+    <div id="contenedor-registro" v-if="blRegistroVisible">
+        <h1>REGISTRO</h1>
 
-    <button @click="presioneLogin">LOGEARSE</button>
-    <button @click="presioneRegistrar">REGISTRARSE</button>
+        <div>
+            <label>USUARIO:</label>
+            <input v-model="sUsuarioRe" type="text"></input>
+        </div>
+
+        <div>
+            <label>PASSWORD:</label>
+            <input v-model="sPasswordRe" type="password"></input>
+        </div>
+
+        <div>
+            <label>REPETIR PASSWORD:</label>
+            <input v-model="sRepetirPasswordRe" type="password"></input>
+        </div>
+
+        <button @click="">ACEPTAR</button>
+        <button @click="">CANCELAR</button>
+    </div>
+    
 
 
 </template>
 
 <style scoped>
+    #contenedor-login{
+        background-color: aquamarine;
+    }
+
 </style>
