da1c1c5da3e8bb05d796708c205727524215983f
Eurekamps <4yony4@gmail.com>
2024-11-05 10:53:29 +0100

    botones con login y registro cambia visibilidad

--- DAM1/src/components/Login.vue
+++ DAM1/src/components/Login.vue
@@ -1,37 +1,87 @@
 <script setup>
-import {ref} from 'vue';
+    import {ref} from 'vue';
 
-const email=ref('');
-const password=ref('');
+    const email=ref('');
+    const password=ref('');
 
-function clickLogear(){
-    window.alert("USUARIO: "+email.value + " PASSWORD: "+password.value);
-}
+    const reemail=ref('');
+    const repassword=ref('');
+    const reRepetirPassword=ref('');
+
+    const blLoginVisible=ref(true);
+    const blRegistroVisible=ref(false);
+
+    function clickLogear(){
+        //window.alert("USUARIO: "+email.value + " PASSWORD: "+password.value);
+    }
+
+    function clickRegistrame(){
+        //window.alert("USUARIO: "+email.value + " PASSWORD: "+password.value);
+        blLoginVisible.value=false;
+        blRegistroVisible.value=true;
+    }
+
+    function clickAceptar(){
+        //window.alert("USUARIO: "+email.value + " PASSWORD: "+password.value);
+    }
+
+    function clickCancelar(){
+        //window.alert("USUARIO: "+email.value + " PASSWORD: "+password.value);
+    }
 
 </script>
 
 <template>
-    <h1> LOGIN PAGE</h1><br/><br/>
 
-    <!-- Email Input -->
-    <div>
-        <label for="email">Email:</label>
-        <input type="email" id="email" v-model="email" />
+    <div id="contenedor-login" v-if="blLoginVisible">
+        <h1> LOGIN PAGE</h1><br/><br/>
+
+        <!-- Email Input -->
+        <div>
+            <label for="email">Email:</label>
+            <input type="email" id="email" v-model="email" />
+        </div>
+
+        <!-- Password Input -->
+        <div>
+            <label for="password">Password:</label>
+            <input type="password" id="password" v-model="password" />
+        </div>
+        <br/>
+
+        <button @click="clickLogear">Logearme</button>
+        <button @click="clickRegistrame">Registrarme</button>
     </div>
 
-    <!-- Password Input -->
-    <div>
-        <label for="password">Password:</label>
-        <input type="password" id="password" v-model="password" />
+    <div id="contenedor-registro" v-if="blRegistroVisible">
+        <h1> REGISTRO PAGE</h1><br/><br/>
+
+        <!-- Email Input -->
+        <div>
+            <label for="email">Email:</label>
+            <input type="email" id="email" v-model="reemail" />
+        </div>
+
+        <!-- Password Input -->
+        <div>
+            <label for="password">Password:</label>
+            <input type="password" id="password" v-model="repassword" />
+        </div>
+
+        <!-- Password Input -->
+        <div>
+            <label for="password">Repetir Password:</label>
+            <input type="password" id="password" v-model="reRepetirPassword" />
+        </div>
+        <br/>
+
+        <button @click="clickLogear">Aceptar</button>
+        <button @click="">Cancelar</button>
     </div>
-    <br/>
-
-    <button @click="clickLogear">Logearme</button>
-
-
 </template>
 
-
 <style scoped>
-
+    #contenedor-login{
+        background-color: antiquewhite;
+    }
 </style>
